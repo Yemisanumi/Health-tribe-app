4513083828694e57f6d2b791ab9358f83d109ed8
Oluwayemi Sanumi <oluwayemisanumi@gmail.com>
2024-04-22 06:49:24 -0700

    added styling for Modal1.css

--- src/LandingPage/Modal1.jsx
+++ src/LandingPage/Modal1.jsx
@@ -2,20 +2,23 @@
 import React from 'react';
 import './Modal1.css';
 import thumbsup from '../assets/thumbsup.png'
+import CLose from '../assets/CLose.svg'
 
-const Modal1 = ({show, onClose }) => {
+const Modal1 = ({ show, onClose }) => {
   return (
     <>
-    {show && (
-    <div className="modal-sect">   
-      <div className="modal-cont">
-        
-        <img className="thumbs-up-image" src= {thumbsup} alt="Thumbs Up" />
-        <p>Your message has been successfully sent!</p>
-      </div>
-    </div>
-  )};
-  </>
+      {show && (
+       <div className="modal">
+       <div className="modal-content">
+         <span className="close" onClick={onClose}>
+         <img src={CLose} alt="Close" />
+         </span>
+         <img className='thumbsup' src= {thumbsup} alt="Thumbs Up" />
+         <p className="text">You have Successfully sent</p>
+       </div>
+     </div>
+      )};
+    </>
   );
 };
 
